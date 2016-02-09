f2d2d5b04bfac2617536e55e28fcead1d9a59bdf
Peter C. Chapin <PChapin@vtc.vsc.edu>
2016-02-09 10:53:40 -0500

    Added the depend tool code base.

---
+++ Tools/depend/linescan.hpp
@@ -0,0 +1,15 @@
+/*! \file    linescan.hpp
+    \brief   Declaration of functions that scan lines looking for #include.
+    \author  Peter C. Chapin <PChapin@vtc.vsc.edu>
+
+*/
+
+#ifndef LINESCAN_HPP
+#define LINESCAN_HPP
+
+extern void handle_line( char *line );
+  // This function figures out if the given line is a #include and, if so, it writes information
+  // into the current dependency list as well as calls handle_file( ) so that this include file
+  // gets scanned.
+
+#endif
---
+++ Tools/depend/splits.cpp
@@ -0,0 +1,66 @@
+/*! \file    splits.cpp
+    \brief   String splitting functions.
+    \author  Peter C. Chapin <PChapin@vtc.vsc.edu>
+
+*/
+
+#include "environ.hpp"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "misc.hpp"
+
+/*!
+ * This function accepts a string and breaks into all of its substrings with one call. It loads
+ * up an array of char * with pointers to the substrings. The original string is modified by the
+ * operation.
+ */
+bool split_string(
+    char *buffer,                   // String to be split.
+    const char *delimiters,         // String of delimiter characters.
+    char *strings[],                // Array to be filled with pntrs to substrings.
+    int   max_number_of_strings,    // Maximum number of substrings allowed.
+    int  *actual_number_of_strings  // Actual number of substrings found.
+  )
+{
+    char *buffer_pointer;         // Temporary pointer into buffer.
+    bool  in_string = false;      // =false when *buf_ptr not pointing at str.
+    bool  return_code = true;     // Returns false if too many substrings.
+
+    // Initialize result and check for out of bounds input.
+    *actual_number_of_strings = 0;
+    if( max_number_of_strings < 0 ) return_code = false;
+    else {
+
+        // Sweep down string until null byte reached.
+        buffer_pointer = buffer;
+        while( *buffer_pointer != '\0' ) {
+
+            // Try to process the current character only if no error so far.
+            if( return_code == true ) {
+
+                // If delimiter found while scanning an argument...
+                if( ( strchr( delimiters, *buffer_pointer ) != NULL ) && ( in_string == true ) ) {
+                    *buffer_pointer = '\0';
+                    in_string = false;
+                }
+
+                // If non delimiter found while scanning "white" space...
+                else if( ( strchr( delimiters, *buffer_pointer ) == NULL ) && ( in_string == false ) ) {
+
+                    // Install only if space. If no space left set error flag.
+                    if( *actual_number_of_strings < max_number_of_strings ) {
+                        strings[*actual_number_of_strings] = buffer_pointer;
+                        ( *actual_number_of_strings )++;
+                        in_string = true;
+                    }
+                    else return_code = false;
+                }
+            }
+            buffer_pointer++;
+        }   // End of while loop.
+    }       // End of overall if... else...
+
+    return return_code;
+}
