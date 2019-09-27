e5e6a15e29efe45dcdc456bb5ff2d267a227ca9b
Peter Chapin <chapinp@acm.org>
2019-09-26 20:41:23 -0400

    Improved the organization of the unit test program.

---
+++ src/check/check.hpp
@@ -0,0 +1,12 @@
+/*! \file    check.hpp
+ *  \brief   Declarations of various unit test functions.
+ *  \author  Peter C. Chapin <chapinp@acm.org>
+ */
+
+#ifndef CHECK_HPP
+#define CHECK_HPP
+
+bool EditBuffer_tests( );
+bool EditList_tests( );
+
+#endif
---
+++ src/check/check_EditList.cpp
@@ -0,0 +1,12 @@
+/*! \file    check_EditList.cpp
+ *  \brief   EditList unit tests.
+ *  \author  Peter C. Chapin <chapinp@acm.org>
+ */
+
+#include "EditList.hpp"
+#include "UnitTestManager.hpp"
+
+bool EditList_tests( )
+{
+     return true;
+}
